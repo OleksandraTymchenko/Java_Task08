82baad6e518a4830b57c4b8d17b49951d3acbc19
OleksandraTymchenko <OleksandraTymchenko@users.noreply.github.com>
2020-02-22 11:22:02 +0200

    Update Main.java

--- src/alextym/com/task08/Main.java
+++ src/alextym/com/task08/Main.java
@@ -5,13 +5,14 @@ import java.util.Scanner;
 import static alextym.com.task08.Books.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         int i = 5;
         Book book1 = new Book(1, "The Old Man and the Sea", "Ernest Hemingway", "B & W Publishing", 1987, 324, 78);
         Book book2 = new Book(2, "A Walk in Amnesia", "O. Henry", "Ballantine Books", 2001, 90, 45);
         Book book3 = new Book(3, "Robin Hood", "Sally M. Stockton", "Ace Books", 1999, 342, 20);
         Book book4 = new Book(4, "A Kiss Before Dying", "Ira Levin", "Airiti Press", 2009, 420, 110);
         Book book5 = new Book(5, "Pride And Prejudice", "Jane Austen", "Armida Publications", 2004, 145, 90);
+
         Books books = new Books(i);
         books.addBook(book1);
         books.addBook(book2);
@@ -22,7 +23,13 @@ public class Main {
 
         System.out.println("Введите процент увеличения стоимости книг: ");
         Scanner sc = new Scanner(System.in);
-        int procent = sc.nextInt();
+        int procent = 0;
+        try {
+            procent = sc.nextInt();
+        } catch (Exception e) {
+            System.out.println("Sorry, enter a number");
+
+        }
         books.changePrice(procent);
         System.out.println("---------------------------");
         books.printBooks();
@@ -37,15 +44,21 @@ public class Main {
 
         System.out.println("---------------------------");
         System.out.println("Введите год: ");
-        int year = sc.nextInt();
+        int year = 0;
+        try {
+            year = sc.nextInt();
+        } catch (Exception e) {
+            System.out.println("Sorry, enter a number");
+
+        }
         Books findBooksAfterYear = new Books(i);
         findBooksAfterYear.setBooks(books.searchBookYear(year));
-        findBooksAfterYear.printBooks();
-        
+
         Book[] booksToCompare = {book1, book2, book3, book4, book5};
         compareAutorNames(booksToCompare);
         comparePuplisherNames(booksToCompare);
         comparePrices(booksToCompare);
 
     }
+
 }
